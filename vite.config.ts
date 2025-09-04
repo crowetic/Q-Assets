9978b5733413e4b6b0e2ba035d0f4dbc9243c2b6
crowetic <jason@crowetic.com>
2025-09-03 20:17:16 -0700

    added code obfuscator, configuration therein, and bumped to 0.45b

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import obfuscator from 'vite-plugin-obfuscator-ts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), obfuscator({
+    obfuscatorOptions: {
+      compact: true,
+      controlFlowFlattening: false,
+      deadCodeInjection: true,
+      debugProtection: false,
+      debugProtectionInterval: 0,
+      disableConsoleOutput: false,
+      identifierNamesGenerator: 'hexadecimal',
+      log: true,
+      numbersToExpressions: false,
+      renameGlobals: false,
+      selfDefending: false,
+      simplify: true,
+      splitStrings: false,
+      stringArray: true,
+      stringArrayCallsTransform: false,
+      stringArrayCallsTransformThreshold: 0.5,
+      stringArrayEncoding: [],
+      stringArrayIndexShift: true,
+      stringArrayRotate: true,
+      stringArrayShuffle: true,
+      stringArrayWrappersCount: 1,
+      stringArrayWrappersChainedCalls: true,
+      stringArrayWrappersParametersMaxCount: 2,
+      stringArrayWrappersType: 'variable',
+      stringArrayThreshold: 0.75,
+      unicodeEscapeSequence: false,
+    },
+  }),],
   base: '',
   optimizeDeps: {
     include: ['@mui/material', '@mui/styled-engine', '@mui/system'],
